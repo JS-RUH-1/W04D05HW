70a04083b5eaef39fa03a838a68aa71b82114653
Mfhy Btrkez <ea@ea.sa>
2021-10-29 21:11:57 +0300

    navbar container

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
 export default function About() {
-    return <div>
+    return <div className="container center mt-5">
         
         <h1>About Us</h1>
+        <p>Instamad is instagram like website made for a homework, used axios</p>
         
         </div>;
 }
